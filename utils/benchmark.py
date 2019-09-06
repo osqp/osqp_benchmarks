Fixed geometric mean (use logs and exp to avoid overflow). Removed slackness check (IP solvers are terrible at this). Disabled QPLIB because they are too large for numpy to handle for now

utils/benchmark.py
@@ -120,7 +120,12 @@ def compute_performance_profiles(solvers, problems_type):
     # plt.show(block=False)
 
 def geom_mean(t, shift=10.):
-    return np.power(np.prod(t + shift), 1/len(t)) - shift
+    """Compute the shifted geometric mean using formula from
+    http://plato.asu.edu/ftp/shgeom.html
+
+    NB. Use logarithms to avoid numeric overflows
+    """
+    return np.exp(np.sum(np.log(np.maximum(1, t + shift))/len(t))) - shift
 
 
 def compute_shifted_geometric_means(solvers, problems_type):
@@ -137,7 +142,8 @@ def compute_shifted_geometric_means(solvers, problems_type):
         # Get total number of problems
         n_problems = len(df)
 
-        t[solver] = df['run_time'].values
+        # NB. Normalize to avoid overflow. They get normalized back anyway.
+        t[solver] = df['run_time'].values * 1e-03
         status[solver] = df['status'].values
 
         # Set maximum time for solvers that did not succeed

utils/general.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.linalg as la
+import solvers.solvers as s
 import errno
 import os
 
@@ -77,11 +78,11 @@ def is_qp_solution_optimal(qp_problem, x, y, high_accuracy=False):
     primal-dual solution (x, y) and the tolerance eps
     '''
     if high_accuracy:
-        eps_abs = 1e-05
-        eps_rel = 1e-05
+        eps_abs = s.eps_high
+        eps_rel = s.eps_high
     else:
-        eps_abs=1e-03
-        eps_rel=1e-03
+        eps_abs=s.eps_low
+        eps_rel=s.eps_low
 
     # Get problem matrices
     P = qp_problem['P']
@@ -113,24 +114,25 @@ def is_qp_solution_optimal(qp_problem, x, y, high_accuracy=False):
               (la.norm(dua_res, np.inf), eps_dua))
         return False
 
-    # Check complementary slackness
-    y_plus = np.maximum(y, 0)
-    y_minus = np.minimum(y, 0)
+    # Check complementary slackness (REMOVED, not compatible with IP methods)
+    #  y_plus = np.maximum(y, 0)
+    #  y_minus = np.minimum(y, 0)
+    #
+    #  eps_comp = eps_abs + eps_rel * np.max([la.norm(Ax, np.inf)])
+    #
+    #  comp_res_u = np.minimum(y_plus, np.abs(u - Ax))
+    #  comp_res_l = np.minimum(-y_minus, np.abs(Ax - l))
+    #
+    #  if la.norm(comp_res_l, np.inf) > eps_comp:
+    #      print("Error in complementary slackness residual l: %.4e > %.4e" %
+    #            (la.norm(comp_res_l, np.inf), eps_comp))
+    #      return False
+    #
+    #  if la.norm(comp_res_u, np.inf) > eps_comp:
+    #      print("Error in complementary slackness residual u: %.4e > %.4e" %
+    #            (la.norm(comp_res_u, np.inf), eps_comp))
+    #      return False
 
-    eps_comp = eps_abs + eps_rel * np.max([la.norm(Ax, np.inf)])
-
-    comp_res_u = np.minimum(y_plus, np.abs(u - Ax))
-    comp_res_l = np.minimum(-y_minus, np.abs(Ax - l))
-
-    if la.norm(comp_res_l, np.inf) > eps_comp:
-        print("Error in complementary slackness residual l: %.4e > %.4e" %
-              (la.norm(comp_res_l, np.inf), eps_comp))
-        return False
-
-    if la.norm(comp_res_u, np.inf) > eps_comp:
-        print("Error in complementary slackness residual u: %.4e > %.4e" %
-              (la.norm(comp_res_u, np.inf), eps_comp))
-        return False
     # If we arrived until here, the solution is optimal
     return True
 